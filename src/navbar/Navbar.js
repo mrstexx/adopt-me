6af1d077b6f2834709448077aa4202f7589e0472
Stefan Miljevic <stefi5miljevic@gmail.com>
2020-07-19 15:28:23 +0200

    add ssr, emotions, code splitting...

---
+++ src/navbar/Navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "@reach/router";
+import { css } from "@emotion/core";
+import colors from '../colors';
+
+const NavBar = () => (
+  <header
+    css={css`
+      background-color: ${colors.secondary};
+      padding: 15px;
+      position: sticky;
+      top: 0;
+      z-index: 10;
+    `}
+  >
+    <Link to="/">Adopt Me!</Link>
+    <span
+      css={css`
+        font-size: 60px;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      `}
+      role="img"
+      aria-label="logo"
+    >
+      🐶
+    </span>
+  </header>
+);
+
+export default NavBar;
