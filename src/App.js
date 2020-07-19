8de5151b4a0a1bd49986c9dd35b31308d0689a2a
Stefan Miljevic <stefi5miljevic@gmail.com>
2020-07-19 15:31:15 +0200

    disable ssr

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState, lazy, Suspense } from "react";
 import { Router } from "@reach/router";
 import ThemeContext from "./context/ThemeContext";
 import NavBar from "./navbar/Navbar";
-import Details from "./details/Details";
-import SearchParams from "./search/SearchParams";
+// import Details from "./details/Details";
+// import SearchParams from "./search/SearchParams";
 
 // render details only when really needed
-// const Details = lazy(() => import("./details/Details"));
-// const SearchParams = lazy(() => import("./search/SearchParams"));
+const Details = lazy(() => import("./details/Details"));
+const SearchParams = lazy(() => import("./search/SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -16,12 +16,12 @@ const App = () => {
       <ThemeContext.Provider value={themeHook}>
         <div>
           <NavBar />
-          {/* <Suspense fallback={<h1>loading route...</h1>}> */}
+          <Suspense fallback={<h1>loading route...</h1>}>
           <Router>
             <SearchParams path="/" />
             <Details path="/details/:id" />
           </Router>
-          {/* </Suspense> */}
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
